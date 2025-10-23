65f1574eef7fa29ad332d56de31c1dc1433ba44e
Sangster422 <andreu.sangstersales@gmail.com>
2025-10-23 09:27:01 +0200

    Update t1.cpp
    Change1

--- t1.cpp
+++ t1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+// Trying to figure out if it works...
 int main() {
     std::cout << "Hello, World!" << std::endl; // Print "Hello, World!" to the console
     return 0; // Indicate that the program ended successfully
